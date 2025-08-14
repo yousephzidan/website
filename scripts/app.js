5f4f71a664834ca1025f5d7606d23f6442ef4436
uzidan0 <yousephzidan10@proton.me>
2025-08-15 02:39:14 +0400

    updated project

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,20 @@ console.log("Sanity Check!");
 
 const gif = document.getElementById('gif');
 
-gif.style.setProperty('background-size', '300%', 'important');
+gif.style.setProperty('background-size', '350%', 'important');
 gif.style.setProperty('background-position', 'top left', 'important');
 gif.style.backgroundPositionY = "top left";
 
 setTimeout(() => {
   gif.style.setProperty('background-size', '200%', 'important');
-   gif.style.setProperty('background-position', 'center center', 'important');
+  gif.style.setProperty('background-position', 'center center', 'important');
 
-}, 20000); // after 2 seconds
+}, 21000); // after 2 seconds
+
+
+setTimeout(() => {
+  gif.style.setProperty('background-size', '400%', 'important');
+  gif.style.setProperty('background-position', 'top left', 'important');
+
+}, 50000); // after 2 seconds
+
